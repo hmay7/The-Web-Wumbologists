c48a1645de7d339fef461c6de7a109c5d49ee4de
Michael Trunk <trunk.6@osu.edu>
2018-10-27 11:25:07 -0400

    Updated routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,53 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+  ##############################################################
+  # ACCOUNT CONTROLS
+  ##############################################################
+  # This will generate /log_in and /sign_up for us
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+  }
+
+  # Profile page viewing a profile for user named :username
+  get '/users/:username'
+
+  # Generates a view which lets user view and change their settings
+  get '/users/:username/settings' 
+
+  # This request is sent by user after they change their settings; then redirect them to profile page
+  put '/users/:username/settings'
+
+  ##############################################################
+  # ACCOUNT-SPECIFIC-CONTROLS
+  ##############################################################
+
+  # Controls for Familys
+  # Show list of nearby shelters which match needs
+  get '/shelters'
+
+  # Controls for shelters
+  # Get list of suppliers which match needs
+  get '/suppliers'
+  # Get list of nearby families to which we may need to be able to help or distribute to
+  get '/families'
+
+  # Controls for suppliers
+  # Get list of shelters in need
+  get '/shelters'
+
+
+
+  ##############################################################
+  # ABOUT / EXTRA RESOURCES / STATIC PAGES
+  ##############################################################
+
+  # About page
+  get '/about'
+
+  # External Resources Page
+  get '/resources'
+
 end
