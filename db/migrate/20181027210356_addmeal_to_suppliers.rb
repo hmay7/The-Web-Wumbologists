547d486f3c34e2e425dfaf08f3ae0c6f423f43de
Ellen DeWitt <dewittellen15@gmail.com>
2018-10-27 17:23:48 -0400

    suppliers

---
+++ db/migrate/20181027210356_addmeal_to_suppliers.rb
@@ -0,0 +1,4 @@
+class AddmealToSuppliers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20181027210707_addgasoline_gallon_to_suppliers.rb
@@ -0,0 +1,4 @@
+class AddgasolineGallonToSuppliers < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
