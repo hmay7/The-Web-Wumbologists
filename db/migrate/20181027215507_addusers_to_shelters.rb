11c1fb1ccb06e3a6376dcbfc25d63e8695552944
Ellen DeWitt <dewittellen15@gmail.com>
2018-10-27 20:22:08 -0400

    table updates

--- db/migrate/20181027215507_addusers_to_shelters.rb
+++
@@ -1,5 +0,0 @@
-class AddusersToShelters < ActiveRecord::Migration[5.2]
-  def change
-  	add_column :shelters, :users, :references
-  end
-end
--- db/migrate/20181027225746_addgallonsgasoline_tosuppliers.rb
+++
@@ -1,5 +0,0 @@
-class AddgallonsgasolineTosuppliers < ActiveRecord::Migration[5.2]
-  def change
-  	add_column :suppliers, :gasoline_gallon, :integer
-  end
-end
