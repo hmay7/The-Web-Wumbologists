4dfcf85f6ab53d133f409cbf7ca4715d6208666d
Michael Trunk <trunk.6@osu.edu>
2018-10-27 21:29:28 -0400

    Added user and user type to sign up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,21 @@
 module ApplicationHelper
 
   def user_profile_path
-    return "/users/profile/#{current_user.username}"
+    return "/users/profile/#{current_user.user_name}"
   end
+
+  def user_is_family?(user)
+  	return user_signed_in? && Family.find_by_user_id(current_user.id) != nil
+  end
+
+  def user_is_supplier?(user)
+    return user_signed_in? && Supplier.find_by_user_id(current_user.id) != nil
+  end
+
+  def user_is_shelter?(user)
+    return user_signed_in? && Shelter.find_by_user_id(current_user.id) != nil
+  end
+
+  
 
 end
