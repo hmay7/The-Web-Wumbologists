452a7242c6b5a5ef7727508bfa9371f301967773
Michael Trunk <trunk.6@osu.edu>
2018-10-28 09:53:02 -0400

    Fixed Shelters and Suppliers Views

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,7 +1,13 @@
 class SuppliersController < ApplicationController
   def show
-  	@suppliers = Supplier.all
-  	@suppliers.sort! do | supp1, supp2 | 
-  		return supp1.distance_to(current_user) <=> supp2.distance_to(current_user)
+  	sup_all = Supplier.all
+  	@suppliers = []
+  	sup_all.each do |s|
+  		@suppliers.push s
+  	end
+
+  	@suppliers.sort! do | supp1, supp2 |
+  		return supp1.user.distance_to(current_user) <=> supp2.user.distance_to(current_user)
+  	end
   end
 end
