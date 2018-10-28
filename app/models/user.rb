68ec6dac8791ddec0d13b795802094006265232f
Ellen DeWitt <dewittellen15@gmail.com>
2018-10-28 07:45:32 -0400

    distance functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def distance_to(user)
+  	return Geocoder::Calculations.distance_between([self.longitude, self.latitude], [user.longitude, user.latitude])
+  end
 end
